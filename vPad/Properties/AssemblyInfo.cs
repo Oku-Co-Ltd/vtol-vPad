dfc9853ab0adb96c2ade37a5688cdc6fb8181090
Kane Bruce <kbruce@mines.edu>
2022-01-01 21:55:32 -0700

    Update to 0.9.8

--- vPad/Properties/AssemblyInfo.cs
+++ vPad/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Oku's vPad")]
+[assembly: AssemblyTitle("vPadOku")]
 [assembly: AssemblyDescription("Adds a virtual portable MFD to aircraft.")]
 [assembly: AssemblyConfiguration("live")]
 [assembly: AssemblyCompany("Omnith LLC")]
-[assembly: AssemblyProduct("VTOLVR_Mod_Boilerplate")]
+[assembly: AssemblyProduct("Oku's vPad")]
 [assembly: AssemblyCopyright("Copyright © okureya 2022")]
 [assembly: AssemblyTrademark("Oku's vPad, gottem")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.1")]
+[assembly: AssemblyVersion("0.9.8.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
